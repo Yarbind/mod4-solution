19a3d5548706abc527e12fcf91dbeaf04d600737
Yarbind <43968484+Yarbind@users.noreply.github.com>
2019-04-20 02:38:38 +0530

    Delete speakGoodbye.js

--- speakGoodbye.js
+++
@@ -1,8 +0,0 @@
-(function(window) {
-    var byeSpeaker = new Object();
-    var speakWord = "Good Bye";
-    byeSpeaker.speak = function speak(names) {
-        console.log(speakWord + " " + names);
-    };
-    window.byeSpeaker = byeSpeaker;
-})(window);
