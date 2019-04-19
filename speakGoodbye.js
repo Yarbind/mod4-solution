5696c7282e4da0a3208ce3eef0578a6cccd32bf0
Yarbin <yarbind98@gmail.com>
2019-04-19 17:19:47 +0530

    upload

--- speakGoodbye.js
+++ speakGoodbye.js
@@ -1,8 +1,8 @@
 (function(window) {
     var byeSpeaker = new Object();
     var speakWord = "Good Bye";
-    byeSpeaker.speak = function speak(name) {
-        console.log(speakWord + " " + name);
+    byeSpeaker.speak = function speak(names) {
+        console.log(speakWord + " " + names);
     };
     window.byeSpeaker = byeSpeaker;
 })(window);
